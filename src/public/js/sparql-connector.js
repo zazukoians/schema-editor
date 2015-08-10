5093a6505dee2dcee2682f540634a4e812aa83a3
danja <danny.ayers@gmail.com>
2015-08-10 02:05:12 +0200

    some progress

---
+++ src/public/js/sparql-connector.js
@@ -0,0 +1,19 @@
+// from spec
+getTriplesForProperty(graphURI, propertyURI)
+
+setEndpoint(URL)
+
+addPropertyToClass(graphURI, class, property)
+
+addClassToProperty(graphURI, property, class)
+
+addTextValue(graphURI, resource, property, value, language)
+
+//////
+addTriple(graphURI, s, p, o)
+
+historyAdd(before, after)
+
+generateSnapshotID()
+
+diff(graphURI, before, after)
