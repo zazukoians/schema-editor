669ef550a2a84c910b6521356a79f624cc96e182
danja <danny.ayers@gmail.com>
2015-08-15 19:11:24 +0200

    tweaks

--- src/public/js/schema-edit.js
+++ src/public/js/schema-edit.js
@@ -2,6 +2,71 @@
 
 /* see http://www.w3.org/TR/sparql11-update/ */
 
+
+
+var populateWithResource = function (uri, callback) { // buildEditor is callback
+    //  console.log("getresource " + uri);
+    // var type = queryString["type"];
+    // console.log("TYPE=" + type);
+
+    var map = {
+        graphURI: SchemaEdit.getGraphURI(),
+        uri: uri
+    };
+
+    var getResourceUrl = SchemaEdit.generateGetUrl(getResourceSparqlTemplate, map);
+
+var buildEditor = function (json) {
+    // console.log("json = " + JSON.stringify(json, null, 4));
+    // return;
+    for (var i = 0; i < json.length; i++) {
+        var current = json[i];
+        var node = $("<div></div>");
+
+        var property = $("<a/>");
+        property.attr("href", current["p"]);
+        property.text(current["p"]);
+        node.append(property);
+
+        var deleteButton = $("<button class='delete'>x</button>");
+        var triple = "<" + SchemaEdit.getCurrentResource() + "> "; // subject
+        triple += "<" + current["p"] + "> "; // predicate/property
+
+        deleteButton.attr("data-triple", triple); // stick resource data in attribute
+        property.append(deleteButton);
+
+        node.append($("<br/>"));
+
+        var value = $("<div>what default?</div>"); // needed for bnodes?
+
+        if (current.type == "literal") {
+                                        console.log("IS LITERAL");
+            value = $("<input type='text' value='" + current["o"] + "'></input>");
+            triple += "\"\"\"" + current["o"] + "\"\"\" ."; // object
+
+        }
+        if (current.type == "uri") {
+            value = $("<a />");
+            value.attr("href", current["o"]);
+            console.log("IS URI");
+            node.append($("<button class='inline'>Change</button>"));
+        }
+        console.log("triple = "+triple);
+        node.append(value);
+
+        value.text(current["o"]);
+
+        //var update = $("<button>Update</button>");
+        //node.append(update);
+
+        $("#currentResource").append("<br/><strong>Property</strong>").append(node);
+    }
+}
+    //  console.log("getResourceUrl = " + getResourceUrl);
+    getJsonForSparqlURL(getResourceUrl, buildEditor);
+    // getDataForURL(handleEntry, getPageUrl);
+};
+
 var SchemaEdit = (function () {
     "use strict";
 
@@ -17,6 +82,12 @@ var SchemaEdit = (function () {
     };
 
     var currentResource;
+
+
+
+
+
+
 
     // This is the public interface of the SchemaEditor module.
     var SchemaEdit = {
@@ -97,10 +168,10 @@ var SchemaEdit = (function () {
         setupButtons: function () {
             // DELETE buttons on schema-edit main page
                         $(".delete").click(function () {
-               
+
             });
-            
-            // EXPORT TURTLE buttons on schema-edit main page 
+
+            // EXPORT TURTLE buttons on schema-edit main page
             $("#turtle").click(function () {
                 var query = "CONSTRUCT {?s ?p ?o } WHERE {?s ?p ?o}";
                 var turtleURL = config.sparqlServerHost + config.sparqlQueryEndpoint + encodeURIComponent(query) + "&output=text";
@@ -131,22 +202,8 @@ var SchemaEdit = (function () {
             });
         },
 
-        getResource: function (uri, callback) {
-            //  console.log("getresource " + uri);
-            // var type = queryString["type"];
-            // console.log("TYPE=" + type);
 
-            var map = {
-                graphURI: SchemaEdit.getGraphURI(),
-                uri: uri
-            };
 
-            var getResourceUrl = SchemaEdit.generateGetUrl(getResourceSparqlTemplate, map);
-
-            //  console.log("getResourceUrl = " + getResourceUrl);
-            getJsonForSparqlURL(getResourceUrl, callback);
-            // getDataForURL(handleEntry, getPageUrl);
-        },
 
         generateGetUrl: function (sparqlTemplate, map) {
             var sparql = sparqlTemplater(sparqlTemplate, map);
