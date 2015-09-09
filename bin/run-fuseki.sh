3654641f9c36421ae7757b5229d8553c77b73f04
danja <danny.ayers@gmail.com>
2015-09-09 13:46:15 +0200

    gitignore

--- bin/run-fuseki.sh
+++ bin/run-fuseki.sh
@@ -1,13 +1,12 @@
 # adjust paths for your setup
 
-export FUSEKI_HOME=../../apache-jena-fuseki-2.3.0
-export FUSEKI_BASE=../../apache-jena-fuseki-2.3.0
+export FUSEKI_HOME=../../fuseki
+export FUSEKI_BASE=../../fuseki
 
-java -Xms2048M -Xmx2048M -Xss4m  -jar ../../apache-jena-fuseki-2.3.0/fuseki-server.jar --verbose --update --config ../data/schemaedit-config.ttl --port=3333
+java -Xms2048M -Xmx2048M -Xss4m  -jar ../../fuseki/fuseki-server.jar --verbose --update --config ../data/schemaedit-config.ttl --port=3333
 
 # --pages ../src/public/
 
 # --pages ../jena-fuseki-1.0.0/pages
 
 # -Xss4m is stack
-
