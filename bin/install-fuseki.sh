2fadcf0c57cd187196a481ae7a973e7229dd5a46
danja <danny.ayers@gmail.com>
2015-09-09 15:34:13 +0200

    tweaks

--- bin/install-fuseki.sh
+++ bin/install-fuseki.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd ../../
+cd ../
 wget -c http://apache.panu.it/jena/binaries/apache-jena-fuseki-2.3.0.tar.gz
 tar xzf apache-jena-fuseki-2.3.0.tar.gz
 mv apache-jena-fuseki-2.3.0 fuseki
@@ -11,4 +11,4 @@ touch fuseki/version_apache-jena-fuseki-2.3.0
 # CORS-enabled version
 mv fuseki/webapp/WEB-INF/web.xml fuseki/webapp/WEB-INF/original_web.xml
 cd -
-cp etc/web_CORS-enabled.xml ../../fuseki/webapp/WEB-INF/web.xml
+cp etc/web_CORS-enabled.xml ../fuseki/webapp/WEB-INF/web.xml
