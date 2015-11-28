0a56ec7b4635176d8986c8108071aa84c2178102
danja <danny.ayers@gmail.com>
2015-11-28 12:32:21 +0100

    tweaking install script

--- bin/install-fuseki.sh
+++ bin/install-fuseki.sh
@@ -2,14 +2,14 @@
 
 cd ../
 
-if [ ! -d "$DIRECTORY" ]; then
+if [ ! -d "fuseki" ]; then
 # install Fuseki
    wget -c https://repository.apache.org/content/repositories/snapshots/org/apache/jena/apache-jena-fuseki/2.3.1-SNAPSHOT/apache-jena-fuseki-2.3.1-20151127.100942-77.tar.gz
    tar xzf apache-jena-fuseki-2.3.1-20151127.100942-77.tar.gz
-   mv apache-jena-fuseki-2.3.1-20151127.100942-77 fuseki
+   mv apache-jena-fuseki-2.3.1-SNAPSHOT fuseki
 
 # a marker for future ref.
-   touch fuseki/VERSION_apache-jena-fuseki-2.3.1-20151127.100942-77
+   touch fuseki/VERSION_apache-jena-fuseki-2.3.1-SNAPSHOT
 
 # CORS-enabled version
 # mv fuseki/webapp/WEB-INF/web.xml fuseki/webapp/WEB-INF/original_web.xml
