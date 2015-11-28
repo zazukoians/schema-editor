01fb0a08bfac70b57b8407a5e3c8b4367039642e
danja <danny.ayers@gmail.com>
2015-11-28 12:05:22 +0100

    tweaking install script

--- bin/install-fuseki.sh
+++ bin/install-fuseki.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
 cd ../
-wget -c https://repository.apache.org/content/repositories/snapshots/org/apache/jena/apache-jena-fuseki/2.3.1-SNAPSHOT/apache-jena-fuseki-2.3.1-20151127.100942-77.tar.gz
-tar xzf apache-jena-fuseki-2.3.1-20151127.100942-77.tar.gz
-mv apache-jena-fuseki-2.3.0 fuseki
+
+if [ ! -d "$DIRECTORY" ]; then
+# install Fuseki
+   wget -c https://repository.apache.org/content/repositories/snapshots/org/apache/jena/apache-jena-fuseki/2.3.1-SNAPSHOT/apache-jena-fuseki-2.3.1-20151127.100942-77.tar.gz
+   tar xzf apache-jena-fuseki-2.3.1-20151127.100942-77.tar.gz
+   mv apache-jena-fuseki-2.3.1-20151127.100942-77 fuseki
 
 # a marker for future ref.
-touch fuseki/version_apache-jena-fuseki-2.3.0
+   touch fuseki/VERSION_apache-jena-fuseki-2.3.1-20151127.100942-77
 
 # CORS-enabled version
-mv fuseki/webapp/WEB-INF/web.xml fuseki/webapp/WEB-INF/original_web.xml
-cd -
-cp etc/web_CORS-enabled.xml ../fuseki/webapp/WEB-INF/web.xml
+# mv fuseki/webapp/WEB-INF/web.xml fuseki/webapp/WEB-INF/original_web.xml
+# cd -
+# cp etc/web_CORS-enabled.xml ../fuseki/webapp/WEB-INF/web.xml
+fi
